f56e32e70e9b3f17295e1f83897d4cf6fe2d9cfe
Mrowr Purr <mrowr.purr@gmail.com>
2025-07-07 23:46:27 -0700

    rm dist

--- dist/nodes/WebSocketTrigger/WebSocketTrigger.node.d.ts
+++
@@ -1,6 +0,0 @@
-import { INodeType, INodeTypeDescription, ITriggerFunctions, ITriggerResponse } from "n8n-workflow";
-export declare class WebSocketTrigger implements INodeType {
-    authPropertyName: string;
-    description: INodeTypeDescription;
-    trigger(this: ITriggerFunctions): Promise<ITriggerResponse>;
-}
--- dist/nodes/WebSocketTrigger/utils.d.ts
+++
@@ -1,7 +0,0 @@
-import type { IncomingMessage } from 'http';
-import type { IDataObject, ICredentialDataDecryptedObject } from 'n8n-workflow';
-export declare class WebSocketAuthorizationError extends Error {
-    responseCode: number;
-    constructor(responseCode: number, message?: string);
-}
-export declare function validateWebSocketAuthentication(req: IncomingMessage, authentication: string, getCredentials: (type: string) => Promise<ICredentialDataDecryptedObject | undefined>): Promise<IDataObject | undefined>;
